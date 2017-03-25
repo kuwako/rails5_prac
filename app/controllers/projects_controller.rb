28688b348b6f7bb9ef998ab729dcd101b6561c65
kuwako <m.kuwako0702@gmail.com>
2017-03-25 13:53:57 +0000

    viewの共通化

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -20,6 +20,19 @@ class ProjectsController < ApplicationController
     end
   end
 
+  def edit 
+    @project = Project.find(params[:id])
+  end
+
+  def update
+    @project = Project.find(params[:id])
+    if @project.update(project_params)
+      redirect_to project_path
+    else
+      return render 'edit'
+    end
+  end
+
   private
 
     def project_params

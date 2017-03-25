be222c16e354087962ae7b4636ead4f6d4a805bb
kuwako <m.kuwako0702@gmail.com>
2017-03-25 13:14:19 +0000

    バリデーション追加

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,6 @@
 class ProjectsController < ApplicationController
   def index
-      @projects = Project.all
+    @projects = Project.all
   end
 
   def show
@@ -10,4 +10,19 @@ class ProjectsController < ApplicationController
   def new
     @project = Project.new
   end
+
+  def create
+    @project = Project.new(project_params)
+    if @project.save
+      redirect_to projects_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def project_params
+      params[:project].permit(:title)
+    end
 end

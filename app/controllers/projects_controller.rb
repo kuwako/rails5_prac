c6163e0a57cee63cdeb0a65b3c963d0055c975d6
kuwako <m.kuwako0702@gmail.com>
2017-03-28 09:04:37 +0000

    controller側一部共通化

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,10 +1,12 @@
 class ProjectsController < ApplicationController
+
+  before_action :set_project, only: [:show, :edit, :update, :destroy]
+
   def index
     @projects = Project.all
   end
 
   def show
-    @project = Project.find(params[:id])
   end
 
   def new
@@ -21,11 +23,9 @@ class ProjectsController < ApplicationController
   end
 
   def edit 
-    @project = Project.find(params[:id])
   end
 
   def update
-    @project = Project.find(params[:id])
     if @project.update(project_params)
       redirect_to project_path
     else
@@ -34,7 +34,6 @@ class ProjectsController < ApplicationController
   end
 
   def destroy
-    @project = Project.find(params[:id])
     @project.destroy
     redirect_to projects_path
   end
@@ -43,5 +42,9 @@ class ProjectsController < ApplicationController
 
     def project_params
       params[:project].permit(:title)
+    end
+
+    def set_project
+      @project = Project.find(params[:id])
     end
 end
